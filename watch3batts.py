ca4dab02d74a90d9b66f2abf63fa1b9ae6a0c18c
sudoer <sudoer@ruatlf.ruatlf>
2025-04-04 15:16:01 -0400

    working as expected

--- watch3batts.py
+++ watch3batts.py
@@ -10,4 +10,14 @@ SERIAL=config[1] # this line of config file should be like /dev/ttyS2
 logfile=open('watch3batts'+time.strftime('%Y%m%d%H%M%S')+'.csv','w')
 
 def grabBatteryCsv(batteryAddress):
+    command = ' '.join(['timeout 5', COMMAND_UTIL, SERIAL, '--csv_status','|tr -d \'\\n\''])
+    response = os.popen(command).read()
+    return response.split(',')
+
+battery1 = grabBatteryCsv('0xFF')
+print(battery1)
+battery2 = grabBatteryCsv('0xDF')
+print(battery2)
+battery3 = grabBatteryCsv('0xEF')
+print(battery3)
 
