015c5c8db5a635218edd5d2b5f54a4a65fbb181f
TrynaThinkOf1 <trynathinkof1@gmail.com>
2025-04-12 18:01:30 -0700

    ZEV use with to open config file

--- watch3batts.py
+++ watch3batts.py
@@ -10,9 +10,11 @@ import os
 
 badData = ['66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66','66']
 
-config=open('watch3batts.conf','r').read().splitlines()
-COMMAND_UTIL=config[0] # this line should be the path to the battery communications executable
-SERIAL=config[1] # this line of config file should be like /dev/ttyS2
+with open('watch3batts.conf','r') as config:
+    config = config.read().splitlines()
+    COMMAND_UTIL=config[0] # this line should be the path to the battery communications executable
+    SERIAL=config[1] # this line of config file should be like /dev/ttyS2
+
 logfile=open('watch3batts'+time.strftime('%Y%m%d%H%M%S')+'.csv','w')
 
 def grabBatteryCsv(batteryAddress):
